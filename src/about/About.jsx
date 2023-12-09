097a2865db666ec85cef874a9aeba02d4ac8006b
Naymul-NN <naymulislam108@gmail.com>
2023-12-09 19:36:28 +0600

    what i love section add

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -7,12 +7,15 @@ import reactImage from "../assets/react.jpg";
 import firebaseImage from "../assets/firebase.png";
 import gitImage from "../assets/git.png";
 import GithubImage from "../assets/github.png";
+import nodeImage from "../assets/node.jpg";
+import expressImage from "../assets/express.png";
+import mongodbImage from "../assets/mongodb.png";
 
 const About = () => {
     return (
         <div>
             <div>
-                <h1 className="text-2xl font-bold py-4">Hello, I am Naymul Islam,</h1>
+                <h1 className="text-2xl font-bold py-4">Professional summery :</h1>
                 <p className="w-full rounded-xl h-[100px] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500  flex justify-center items-center">A passionate front-end developer dedicated to crafting engaging and seamless digital experiences. With a background in 2 year learn programing , I thrive on turning creative ideas into functional, user-friendly websites. My expertise lies in harnessing the power of HTML, CSS, and JavaScript to bring designs to life.
                 </p>
             </div>
@@ -52,6 +55,25 @@ const About = () => {
                         <h1 className="font-bold pl-2">Github</h1>
                     </div>
                 </div>
+                <h1 className="text-center  text-3xl font-bold pt-10">Familiar with</h1>
+                <div className=" pt-7 flex justify-evenly items-center ">
+                    <div>
+                        <img className="w-16 h-16" src={nodeImage} alt="" />
+                        <h1 className="font-bold pl-2">Node.js</h1>
+                    </div>
+                    <div>
+                        <img className="w-16 h-16" src={expressImage} alt="" />
+                        <h1 className="font-bold pl-2">Node.js</h1>
+                    </div>
+                    <div>
+                        <img className="w-16 h-16" src={mongodbImage} alt="" />
+                        <h1 className="font-bold pl-2">Node.js</h1>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1 className="text-2xl font-bold pt-7 pb-3">What I Love About Front-End Development:</h1>
+                <p className="w-full rounded-xl h-[100px] bg-gradient-to-r from-blue-300 via-purple-500 to-pink-300  flex justify-center items-center">In the realm of front-end development, I find my true passion lies in the harmonious fusion of creativity and technology. Crafting visually stunning and intuitive user interfaces is not just a job for me; it is a form of artistic expression. I relish the challenge of transforming static designs into dynamic, interactive web experiences that captivate and engage users.</p>
             </div>
         </div>
     );
